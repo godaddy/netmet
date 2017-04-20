a17f96a920c50fe70fc613dec509d9042fcac024
Boris Pavlovic <bpavlovic@godaddy.com>
2017-04-20 11:52:19 -0700

    Improve logging add timestamps
    - Add timestamps to log messages
    - Log sigint, sigterm actions

--- netmet/run.py
+++ netmet/run.py
@@ -11,9 +11,13 @@ from netmet.server import main as server_main
 from netmet.utils import asyncer
 
 
+LOG = logging.getLogger(__name__)
+
+
 def load():
     level = logging.DEBUG if os.getenv("DEBUG") else logging.INFO
-    logging.basicConfig(level=level)
+    logging.basicConfig(level=level,
+                        format='%(asctime)s %(levelname)-8s %(message)s')
 
     if not os.getenv("APP") or os.getenv("APP") not in ["server", "client"]:
         raise ValueError("Set APP env variable to 'server' or 'client'")
@@ -28,12 +32,17 @@ def load():
     app.port = port
 
     def die(*args, **kwargs):
+        LOG.info("Stopping netmet %s" % os.getenv("APP"))
         if os.getenv("APP") == "server":
+            LOG.info("Stopping HTTP server")
             http_server.stop()
+        LOG.info("Joining internal threads")
         mode.die()
         asyncer.die()
         if os.getenv("APP") == "client":
+            LOG.info("Stopping HTTP server")
             http_server.stop()
+        LOG.info("Bye Bye!")
 
     signal.signal(signal.SIGTERM, die)
     signal.signal(signal.SIGINT, die)
